9599ccfd33026657b1a5b3332747308d6c376c71
Pauline Cha <pacha@davidson.edu>
2023-02-21 13:41:29 -0500

    questionnaire added a couple (first 5)

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,5 @@
 let instagram = ["assets/img/choiwoosik.PNG", "assets/img/patrickdempsey.PNG"];
 
-// var elem = document.createElement("img");
-// elem.setAttribute("src", "assets/img/choiwoosik.png");
-// elem.setAttribute("width", "100%");
-// var elem1 = document.createElement("img");
-// elem1.setAttribute("src", "assets/img/patrickdempsey.png");
-// elem1.setAttribute("width", "100%");
-
-// let instaElems = [];
-
-// instagram.forEach(e => {
-//     var elem = document.createElement("img");
-//     elem.setAttribute("src", e);
-//     elem.setAttribute("width", "100%");
-//     instaElems[e] = elem;
-// });
-
 const random = Math.floor(Math.random() * 2)
 
 let inststr =""
@@ -25,6 +9,4 @@ inststr += `
 
 $("#insta-pic").html(inststr);
 
-
-// document.getElementById("insta-pic").appendChild(instaElems[random]);
 
